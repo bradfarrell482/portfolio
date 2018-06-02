5e2d0011f9d58f6d8c3ddfde6f77773176d1c914
Kevin Amorim <Kramorim@bu.edu>
2018-06-02 05:37:08 -0500

    Fixed NavBar to have a hamburger menu
    Made a bunch of changes which ultimately caused the navbar to have the toggle menu hamburger button on screen sizes 700px and smaller.
    Also adjusted the nav bar to layout according to mid sizes.

--- js/homepage.js
+++ js/homepage.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
     $("#nav").load("/html/nav.html");
 });
+
+function toggleNav() {
+    if ($("#navcontent ul").hasClass("show")){
+        $("#navcontent ul").removeClass("show");
+    } else {
+        $("#navcontent ul").addClass("show");
+    }
+    return;
+}
