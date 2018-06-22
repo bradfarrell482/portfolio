8d5344d727e7db227ee13d335f02357858774b3f
bradfarrell482 <bradfarrell482@gmail.com>
2018-06-22 06:05:55 -0500

    Added copy to Scout
    Initial draft of text for Scout

--- js/project_page.js
+++ js/project_page.js
@@ -1,16 +1,38 @@
 $(document).on("scroll", function () {
-    var pageTop = $(document).scrollTop()
-    var pageBottom = pageTop + $(window).height()
-    var tags = $(".module")
-  
-    for (var i = 0; i < tags.length; i++) {
-      var tag = tags[i]
-  
-      if ($(tag).position().top < pageBottom) {
-        $(tag).addClass("visible")
-      } else {
-        $(tag).removeClass("visible")
-      }
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".module");
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
     }
-  })
+  }
+});
+
+$(document).ready(function(){
+  $(".toggle_expand").on("click", function () {
+    console.log("Expanding Box");
+    $(this).addClass("toggle_collapse hidden");
+    $(this).removeClass("toggle_expand");
+    $(".intro").removeClass("collapsed"); 
+    $(".fadeout").addClass("hidden");
+  });
+
+  $(".toggle_collapse").on("click", function () {
+    console.log("Collapsing Box");
+    $(this).addClass("toggle_expand");
+    $(this).removeClass("toggle_collapse");
+    $(".intro").addClass("collapsed"); 
+  });
+
+
+
+});
+
+
 
